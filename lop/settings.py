6cb72facd7f7eae9caad23c31d5463b07444f3a1
Corey Farwell <coreyf@rwell.org>
2015-11-02 22:41:24 -0500

    Add basic auth via django-browserid

--- lop/settings.py
+++ lop/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_browserid',
+)
+
+AUTHENTICATION_BACKENDS = (
+   'django.contrib.auth.backends.ModelBackend',
+   'django_browserid.auth.BrowserIDBackend',
 )
 
 MIDDLEWARE_CLASSES = (
