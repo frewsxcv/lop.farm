efd8ea247f426c02bcb311f0ed14bcffed7d7730
Corey Farwell <coreyf@rwell.org>
2015-11-05 18:38:46 -0500

    Move URL routes into associated Django app module

--- lop/urls.py
+++ lop/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', 'landing.views.landing'),
-    url(r'^run/$', 'run.views.run'),
+    url(r'', include('landing.urls')),
+    url(r'', include('run.urls')),
     url(r'', include('django_browserid.urls')),
 ]
