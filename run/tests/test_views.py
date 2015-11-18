47139f7b5b6e89bb340ba1a60f72aa0307558fb4
Corey Farwell <coreyf@rwell.org>
2015-11-18 08:25:15 -0500

    Run AFL in Celery task instead of web process

--- run/tests/test_views.py
+++ run/tests/test_views.py
@@ -1,10 +1,12 @@
 from django.core.urlresolvers import reverse
 from django.test import TestCase
+from django.test.utils import override_settings
 
 from run.models import AflRun
 
 
 class RunViewTestCase(TestCase):
+    @override_settings(CELERY_ALWAYS_EAGER=True)
     def test_run_view(self):
         url = reverse('run')
         res = self.client.get(url)
