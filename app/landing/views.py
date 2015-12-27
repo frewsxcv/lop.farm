f6646b2949b51a3475f86fb229cf80bafc76d99f
Corey Farwell <coreyf@rwell.org>
2015-12-27 14:42:12 -0800

    Display active and pending runs on landing page

--- app/landing/views.py
+++ app/landing/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 
-from run.models import AflRun
+from run.models import AflRun, Run
 
 
 def landing(request):
-    runs = AflRun.objects.order_by('-start_time')
+    pending_runs = Run.objects.filter(started_on=None, completed_on=None)
+    active_runs = Run.objects.exclude(started_on=None).filter(completed_on=None)
+    afl_runs = AflRun.objects.order_by('-start_time')
     return render(request, 'landing.html', {
-        "runs": runs,
+        "afl_runs": afl_runs,
+        "active_runs": active_runs,
+        "pending_runs": pending_runs,
     })
