bb0a783a393f7054222fccbc4a03341605cf2f20
Corey Farwell <coreyf@rwell.org>
2015-12-27 14:49:09 -0800

    Flake8 fix

--- app/landing/views.py
+++ app/landing/views.py
@@ -5,7 +5,8 @@ from run.models import AflRun, Run
 
 def landing(request):
     pending_runs = Run.objects.filter(started_on=None, completed_on=None)
-    active_runs = Run.objects.exclude(started_on=None).filter(completed_on=None)
+    active_runs = (Run.objects.exclude(started_on=None)
+                              .filter(completed_on=None))
     afl_runs = AflRun.objects.order_by('-start_time')
     return render(request, 'landing.html', {
         "afl_runs": afl_runs,
