4e74cc8a48a7e4a810727c16c443e4b6e07c603e
Corey Farwell <coreyf@rwell.org>
2016-01-14 14:12:45 -0500

    Add page to view specific run instance

--- app/run/urls.py
+++ app/run/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import url
 from run import views
 
 urlpatterns = [
-    url(r'^run/$', views.run, name='run'),
+    url(r'^trigger_run/$', views.trigger_run, name='trigger_run'),
+    url(r'^run/(?P<run_id>[0-9]+)/$', views.run, name='run'),
 ]
