affe2004be94b2ea110926ce9fa685a8681a6bf5
HeraticAssassin <jaynan.alvarez@gmail.com>
2017-05-08 20:48:24 -0500

    Update settings.py

--- app/lop_farm/settings.py
+++ app/lop_farm/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = (
     'django.contrib.admin',
     'django.contrib.auth',
+    'oauth2_provider',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
